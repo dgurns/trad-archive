09f9778ce9994d5f08a08889eb154c1ceefb6ba4
Dan Gurney <gurney.dan@gmail.com>
2021-03-11 12:46:14 +0000

    Try setting up stuff once

--- api/src/apolloServer.ts
+++ api/src/apolloServer.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-lambda';
-import { getConnectionManager } from 'typeorm';
+import { getConnectionManager, Connection } from 'typeorm';
 import { buildSchema } from 'type-graphql';
 import {
   APIGatewayProxyCallback,
@@ -26,68 +26,81 @@ import { PlaceResolver } from 'resolvers/PlaceResolver';
 
 const { SERVERLESS_STAGE } = process.env;
 
-const dbConnectionManager = getConnectionManager();
-const dbIsConnected = dbConnectionManager.has(DB_CONNECTION_NAME);
-console.log('starting up Lambda, dbIsConnected?', dbIsConnected);
+// Connect to DB
+let dbConnection: Connection | undefined;
+console.log('lambda running - dbConnection?', dbConnection);
+if (!dbConnection) {
+  connectToDatabase()
+    .then(() => {
+      console.log('gonna connect');
+      dbConnection = getConnectionManager().get(DB_CONNECTION_NAME);
+      console.log('got connection, it is:', dbConnection);
+    })
+    .catch((error) => console.log(error));
+}
 
-const createServer = async () => {
-  console.log('creating server, dbIsConnected?', dbIsConnected);
-  if (!dbIsConnected) {
-    console.log('not connected, connect');
-    await connectToDatabase();
-  }
+console.log('gonna createServer');
 
-  const schema = await buildSchema({
-    resolvers: [
-      AuthResolver,
-      UserResolver,
-      TagResolver,
-      RelationshipResolver,
-      CommentResolver,
-      CollectionEntryResolver,
-      EntityResolver,
-      AudioItemResolver,
-      PersonResolver,
-      InstrumentResolver,
-      PlaceResolver,
-    ],
-    dateScalarMode: 'isoDate',
-    authChecker,
-    authMode: 'null',
-  });
+// Create server
+let apolloServer: ApolloServer | undefined;
+buildSchema({
+  resolvers: [
+    AuthResolver,
+    UserResolver,
+    TagResolver,
+    RelationshipResolver,
+    CommentResolver,
+    CollectionEntryResolver,
+    EntityResolver,
+    AudioItemResolver,
+    PersonResolver,
+    InstrumentResolver,
+    PlaceResolver,
+  ],
+  dateScalarMode: 'isoDate',
+  authChecker,
+  authMode: 'null',
+})
+  .then((schema) => {
+    console.log('schema built, create server');
+    apolloServer = new ApolloServer({
+      schema,
+      plugins: apolloServerPlugins,
+      context: ({ event, context }) => createCustomContext(event, context),
+    });
+  })
+  .catch((error) => console.log(error));
 
-  const apolloServer = new ApolloServer({
-    schema,
-    plugins: apolloServerPlugins,
-    context: ({ event, context }) => createCustomContext(event, context),
-  });
-
-  return apolloServer;
-};
+console.log('dbConnection and server', dbConnection, apolloServer);
 
 export const graphqlHandler = (
   event: APIGatewayProxyEvent,
   context: LambdaContext,
   callback: APIGatewayProxyCallback
 ) => {
-  createServer().then((server) => {
-    let allowedOrigin;
-    switch (SERVERLESS_STAGE) {
-      case 'dev':
-        allowedOrigin = 'http://localhost:3000';
-        break;
-      case 'prod':
-        allowedOrigin = 'https://www.tradarchive.com';
-        break;
-      default:
-        allowedOrigin = `https://trad-archive-git-${SERVERLESS_STAGE}-dangurney.vercel.app`;
-    }
-    const handler = server.createHandler({
-      cors: {
-        origin: allowedOrigin,
-        credentials: true,
-      },
-    });
-    return handler(event, context, callback);
+  let allowedOrigin;
+  switch (SERVERLESS_STAGE) {
+    case 'dev':
+      allowedOrigin = 'http://localhost:3000';
+      break;
+    case 'prod':
+      allowedOrigin = 'https://www.tradarchive.com';
+      break;
+    default:
+      allowedOrigin = `https://trad-archive-git-${SERVERLESS_STAGE}-dangurney.vercel.app`;
+  }
+  if (
+    typeof dbConnection === 'undefined' ||
+    typeof apolloServer === 'undefined'
+  ) {
+    console.log('whoops no DB or server!');
+    return callback(new Error('Error initializing Lambda'));
+  }
+  const handler = apolloServer.createHandler({
+    cors: {
+      origin: allowedOrigin,
+      credentials: true,
+    },
   });
+  return handler(event, context, callback);
 };

364192e44f86c9374c562da40796506f2dc9001f
Dan Gurney <gurney.dan@gmail.com>
2021-03-13 15:25:48 +0000

    Add preview warning

--- web/components/Layout.tsx
+++ web/components/Layout.tsx
@@ -1,13 +1,25 @@
 import { React } from '@ungap/global-this';
 import Header from 'components/Header';
 
+const { NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF } = process.env;
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: Props) => {
+  const shouldShowPreviewWarning =
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF !== 'master';
+  const branchName = NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF
+    ? `Branch name: ${NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}`
+    : undefined;
+
   return (
     <div>
+      {shouldShowPreviewWarning && (
+        <div className="flex flex-row items-center justify-center p-1 bg-black text-white">
+          This is a preview version of the site with fake data. {branchName}
+        </div>
+      )}
       <Header />
       <div className="flex flex-col justify-start items-center">
         <div className="w-full lg:max-w-5xl py-6 px-4 pb-44">{children}</div>

ef8fbf3f0f2539efbe1a82b4dd9d746fa05867a6
Dan Gurney <gurney.dan@gmail.com>
2021-04-01 10:41:02 +0100

    Add field for time marker

--- api/src/models/Tag.ts
+++ api/src/models/Tag.ts
@@ -1,94 +1,97 @@
 import {
-  Entity as TypeOrmEntity,
-  BaseEntity as TypeOrmBaseEntity,
-  PrimaryGeneratedColumn,
-  ManyToOne,
-  CreateDateColumn,
-  UpdateDateColumn,
-  AfterLoad,
-} from 'typeorm';
-import { ObjectType, Field } from 'type-graphql';
+	Entity as TypeOrmEntity,
+	BaseEntity as TypeOrmBaseEntity,
+	PrimaryGeneratedColumn,
+	ManyToOne,
+	CreateDateColumn,
+	UpdateDateColumn,
+	AfterLoad,
+} from "typeorm";
+import { ObjectType, Field, Int } from "type-graphql";
 
-import { User } from 'models/User';
-import { AudioItem } from 'models/entities/AudioItem';
-import { Person } from 'models/entities/Person';
-import { Instrument } from 'models/entities/Instrument';
-import { Place } from 'models/entities/Place';
-import { Relationship } from 'models/Relationship';
-import { EntityUnion, Entity } from 'resolvers/EntityResolver';
+import { User } from "models/User";
+import { AudioItem } from "models/entities/AudioItem";
+import { Person } from "models/entities/Person";
+import { Instrument } from "models/entities/Instrument";
+import { Place } from "models/entities/Place";
+import { Relationship } from "models/Relationship";
+import { EntityUnion, Entity } from "resolvers/EntityResolver";
 
 // Tag represents a connection between two entities and specifies the
 // relationship between them.
 @ObjectType()
 @TypeOrmEntity()
 export class Tag extends TypeOrmBaseEntity {
-  @Field(() => String)
-  @PrimaryGeneratedColumn('uuid')
-  readonly id!: string;
+	@Field(() => String)
+	@PrimaryGeneratedColumn("uuid")
+	readonly id!: string;
 
-  @Field(() => Relationship)
-  @ManyToOne(() => Relationship, { eager: true })
-  relationship!: Relationship;
+	@Field(() => Relationship)
+	@ManyToOne(() => Relationship, { eager: true })
+	relationship!: Relationship;
 
-  @ManyToOne(() => AudioItem, { nullable: true })
-  subjectAudioItem!: AudioItem;
+	@ManyToOne(() => AudioItem, { nullable: true })
+	subjectAudioItem!: AudioItem;
 
-  @ManyToOne(() => Person, { nullable: true })
-  subjectPerson!: Person;
+	@ManyToOne(() => Person, { nullable: true })
+	subjectPerson!: Person;
 
-  @ManyToOne(() => Instrument, { nullable: true })
-  subjectInstrument!: Instrument;
+	@ManyToOne(() => Instrument, { nullable: true })
+	subjectInstrument!: Instrument;
 
-  @ManyToOne(() => Place, { nullable: true })
-  subjectPlace!: Place;
+	@ManyToOne(() => Place, { nullable: true })
+	subjectPlace!: Place;
 
-  @ManyToOne(() => AudioItem, { nullable: true })
-  objectAudioItem!: AudioItem;
+	@ManyToOne(() => AudioItem, { nullable: true })
+	objectAudioItem!: AudioItem;
 
-  @ManyToOne(() => Person, { nullable: true })
-  objectPerson!: Person;
+	@ManyToOne(() => Person, { nullable: true })
+	objectPerson!: Person;
 
-  @ManyToOne(() => Instrument, { nullable: true })
-  objectInstrument!: Instrument;
+	@ManyToOne(() => Instrument, { nullable: true })
+	objectInstrument!: Instrument;
 
-  @ManyToOne(() => Place, { nullable: true })
-  objectPlace!: Place;
+	@ManyToOne(() => Place, { nullable: true })
+	objectPlace!: Place;
 
-  @Field(() => User)
-  @ManyToOne(() => User, { eager: true })
-  createdByUser!: User;
+	@Field(() => Int, { nullable: true })
+	subjectTimeMarkerSeconds!: number;
 
-  @Field()
-  @CreateDateColumn({ type: 'timestamptz' })
-  createdAt!: Date;
+	@Field(() => User)
+	@ManyToOne(() => User, { eager: true })
+	createdByUser!: User;
 
-  @Field()
-  @UpdateDateColumn({ type: 'timestamptz' })
-  updatedAt!: Date;
+	@Field()
+	@CreateDateColumn({ type: "timestamptz" })
+	createdAt!: Date;
 
-  // Generated fields make this model easier to consume via the GraphQL API.
-  // These fields are not saved in the database and are generated at runtime.
-  // `subjectEntity` is nullable because if you're getting tags for an entity,
-  // you wouldn't need to return the subject entity itself.
-  @Field(() => EntityUnion, { nullable: true })
-  subjectEntity!: Entity;
-  @AfterLoad()
-  setSubjectEntity() {
-    this.subjectEntity =
-      this.subjectAudioItem ??
-      this.subjectPerson ??
-      this.subjectInstrument ??
-      this.subjectPlace;
-  }
+	@Field()
+	@UpdateDateColumn({ type: "timestamptz" })
+	updatedAt!: Date;
 
-  @Field(() => EntityUnion)
-  objectEntity!: Entity;
-  @AfterLoad()
-  setObjectEntity() {
-    this.objectEntity =
-      this.objectAudioItem ??
-      this.objectPerson ??
-      this.objectInstrument ??
-      this.objectPlace;
-  }
+	// Generated fields make this model easier to consume via the GraphQL API.
+	// These fields are not saved in the database and are generated at runtime.
+	// `subjectEntity` is nullable because if you're getting tags for an entity,
+	// you wouldn't need to return the subject entity itself.
+	@Field(() => EntityUnion, { nullable: true })
+	subjectEntity!: Entity;
+	@AfterLoad()
+	setSubjectEntity() {
+		this.subjectEntity =
+			this.subjectAudioItem ??
+			this.subjectPerson ??
+			this.subjectInstrument ??
+			this.subjectPlace;
+	}
+
+	@Field(() => EntityUnion)
+	objectEntity!: Entity;
+	@AfterLoad()
+	setObjectEntity() {
+		this.objectEntity =
+			this.objectAudioItem ??
+			this.objectPerson ??
+			this.objectInstrument ??
+			this.objectPlace;
+	}
 }
--- web/types/index.ts
+++ web/types/index.ts
@@ -1,110 +1,111 @@
 export enum UserPermission {
-  User = 'USER',
-  Admin = 'ADMIN',
+	User = "USER",
+	Admin = "ADMIN",
 }
 
 export interface User {
-  id: string;
-  permissions?: UserPermission[];
-  email?: string;
-  username: string;
-  createdAt: string;
-  updatedAt: string;
+	id: string;
+	permissions?: UserPermission[];
+	email?: string;
+	username: string;
+	createdAt: string;
+	updatedAt: string;
 }
 
 // Entity defines a union of all the different Entity types
 export type Entity = AudioItem | Person | Instrument | Place;
 
 export enum EntityType {
-  AudioItem = 'AudioItem',
-  Person = 'Person',
-  Instrument = 'Instrument',
-  Place = 'Place',
+	AudioItem = "AudioItem",
+	Person = "Person",
+	Instrument = "Instrument",
+	Place = "Place",
 }
 
 export function isAudioItem(entity: Entity): entity is AudioItem {
-  return (entity as AudioItem).entityType === EntityType.AudioItem;
+	return (entity as AudioItem).entityType === EntityType.AudioItem;
 }
 export function isPerson(entity: Entity): entity is Person {
-  return (entity as Person).entityType === EntityType.Person;
+	return (entity as Person).entityType === EntityType.Person;
 }
 export function isInstrument(entity: Entity): entity is Instrument {
-  return (entity as Instrument).entityType === EntityType.Instrument;
+	return (entity as Instrument).entityType === EntityType.Instrument;
 }
 export function isPlace(entity: Entity): entity is Place {
-  return (entity as Place).entityType === EntityType.Place;
+	return (entity as Place).entityType === EntityType.Place;
 }
 
 interface BaseEntity {
-  id: string;
-  name: string;
-  slug: string;
-  aliases: string;
-  description: string | null;
-  tags: Tag[];
-  createdByUser: User;
-  lastUpdatedByUser: User;
-  createdAt: string;
-  updatedAt: string;
+	id: string;
+	name: string;
+	slug: string;
+	aliases: string;
+	description: string | null;
+	tags: Tag[];
+	createdByUser: User;
+	lastUpdatedByUser: User;
+	createdAt: string;
+	updatedAt: string;
 }
 
 export interface AudioItem extends BaseEntity {
-  entityType: EntityType.AudioItem;
-  comments?: Comment[] | null;
-  commentsCount?: number;
-  urlSource: string;
-  isAddedToCollection?: boolean;
+	entityType: EntityType.AudioItem;
+	comments?: Comment[] | null;
+	commentsCount?: number;
+	urlSource: string;
+	isAddedToCollection?: boolean;
 }
 
 export interface Person extends BaseEntity {
-  entityType: EntityType.Person;
-  firstName: string;
-  middleName: string | null;
-  lastName: string;
+	entityType: EntityType.Person;
+	firstName: string;
+	middleName: string | null;
+	lastName: string;
 }
 
 export interface Instrument extends BaseEntity {
-  entityType: EntityType.Instrument;
+	entityType: EntityType.Instrument;
 }
 
 export interface Place extends BaseEntity {
-  entityType: EntityType.Place;
-  latitude: number;
-  longitude: number;
+	entityType: EntityType.Place;
+	latitude: number;
+	longitude: number;
 }
 
 export interface Relationship {
-  id: string;
-  name: string;
-  subjectEntityType: EntityType;
-  objectEntityType: EntityType;
-  createdByUser: User;
-  createdAt: string;
-  updatedAt: string;
+	id: string;
+	name: string;
+	subjectEntityType: EntityType;
+	objectEntityType: EntityType;
+	createdByUser: User;
+	createdAt: string;
+	updatedAt: string;
 }
 
 export interface Tag {
-  id: string;
-  relationship: Relationship;
-  subjectEntity: Entity;
-  objectEntity: Entity;
-  createdByUser: User;
-  createdAt: string;
-  updatedAt: string;
+	id: string;
+	relationship: Relationship;
+	subjectEntity?: Entity;
+	objectEntity: Entity;
+	subjectTimeMarkerSeconds?: number;
+	createdByUser: User;
+	createdAt: string;
+	updatedAt: string;
 }
 
 export interface Comment {
-  id: string;
-  parentAudioItem?: AudioItem | null;
-  text: string;
-  createdByUser: User;
-  createdAt: string;
-  updatedAt: string;
+	id: string;
+	parentAudioItem?: AudioItem | null;
+	text: string;
+	createdByUser: User;
+	createdAt: string;
+	updatedAt: string;
 }
 
 export interface CollectionEntry {
-  id: string;
-  audioItem?: AudioItem | null;
-  user?: User | null;
-  createdAt: string;
+	id: string;
+	audioItem?: AudioItem | null;
+	user?: User | null;
+	createdAt: string;
 }

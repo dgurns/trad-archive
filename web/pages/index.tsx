71232016b68cfefb49bfa0b5cf2fbc6bbca44733
Dan Gurney <gurney.dan@gmail.com>
2021-05-08 17:04:03 +0100

    Prefetch audio items for home page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,48 +1,90 @@
-import useAudioItems from 'hooks/useAudioItems';
+import { apolloClient } from "apolloClient";
+import { AudioItem } from "types";
+import useAudioItems, { AUDIO_ITEMS_QUERY } from "hooks/useAudioItems";
 
-import Layout from 'components/Layout';
-import AudioItem from 'components/AudioItem';
-import LoadingBlock from 'components/LoadingBlock';
+import Layout from "components/Layout";
+import AudioItemComponent from "components/AudioItem";
+import LoadingBlock from "components/LoadingBlock";
 
-export default function Home() {
-  const [audioItems, { loading, error }, fetchNextPage] = useAudioItems({
-    resultsPerPage: 10,
-  });
+const RESULTS_PER_PAGE = 10;
 
-  if (!audioItems && !error) {
-    return (
-      <Layout>
-        <LoadingBlock />
-      </Layout>
-    );
-  } else if (!audioItems && error) {
-    return (
-      <Layout>
-        <div className="text-red-600">{error.message}</div>
-      </Layout>
-    );
-  } else if (!audioItems?.length) {
-    return (
-      <Layout>
-        <div className="text-gray-500">No Audio Items found</div>
-      </Layout>
-    );
-  }
+interface AudioItemsQueryData {
+	audioItems: AudioItem[];
+}
+interface AudioItemsQueryVariables {
+	input: {
+		take: number;
+		skip: number;
+	};
+}
+export async function getStaticProps() {
+	const { data } = await apolloClient.query<
+		AudioItemsQueryData,
+		AudioItemsQueryVariables
+	>({
+		query: AUDIO_ITEMS_QUERY,
+		variables: {
+			input: {
+				take: RESULTS_PER_PAGE,
+				skip: 0,
+			},
+		},
+	});
+	return {
+		props: {
+			prefetchedAudioItems: data?.audioItems ?? null,
+		},
+		revalidate: 1,
+	};
+}
 
-  return (
-    <Layout>
-      {audioItems.map((audioItem, index) => (
-        <AudioItem audioItem={audioItem} key={index} />
-      ))}
-      {!loading ? (
-        <div className="flex flex-row justify-center">
-          <button className="btn-text" onClick={fetchNextPage}>
-            Load More
-          </button>
-        </div>
-      ) : (
-        <LoadingBlock />
-      )}
-    </Layout>
-  );
+interface Props {
+	prefetchedAudioItems: AudioItem[] | null;
+}
+
+export default function Home({ prefetchedAudioItems }: Props) {
+	const [
+		audioItems = prefetchedAudioItems,
+		{ loading, error },
+		fetchNextPage,
+	] = useAudioItems({
+		resultsPerPage: RESULTS_PER_PAGE,
+	});
+
+	if (!audioItems && !error) {
+		return (
+			<Layout>
+				<LoadingBlock />
+			</Layout>
+		);
+	} else if (!audioItems && error) {
+		return (
+			<Layout>
+				<div className="text-red-600">{error.message}</div>
+			</Layout>
+		);
+	} else if (!audioItems?.length) {
+		return (
+			<Layout>
+				<div className="text-gray-500">No Audio Items found</div>
+			</Layout>
+		);
+	}
+
+	return (
+		<Layout>
+			{audioItems.map((audioItem, index) => (
+				<AudioItemComponent audioItem={audioItem} key={index} />
+			))}
+			{!loading ? (
+				<div className="flex flex-row justify-center">
+					<button className="btn-text" onClick={fetchNextPage}>
+						Load More
+					</button>
+				</div>
+			) : (
+				<LoadingBlock />
+			)}
+		</Layout>
+	);
 }

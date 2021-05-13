2188e935460a57b761073222151911e9812c448c
Dan Gurney <gurney.dan@gmail.com>
2021-05-13 10:44:39 +0100

    Speed up loading for Google fonts

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -1,39 +1,50 @@
 import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from 'next/document';
+	Html,
+	Head,
+	Main,
+	NextScript,
+	DocumentContext,
+} from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+	static async getInitialProps(ctx: DocumentContext) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            rel="stylesheet preconnect"
-            href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body className="bg-gray-100">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="icon" href="/favicon.ico" />
+					<link rel="dns-prefetch" href="//fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com/" />
+					<link
+						href="https://fonts.googleapis.com/icon?family=Material+Icons"
+						rel="preload"
+						as="stylesheet"
+					/>
+					<link
+						href="https://fonts.googleapis.com/icon?family=Material+Icons"
+						rel="stylesheet"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap"
+						rel="reload"
+						as="stylesheet"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
+				<body className="bg-gray-100">
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;

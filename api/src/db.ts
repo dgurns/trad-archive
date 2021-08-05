4fb9a9eeb06ed7a337df6ac4d371f1ef40d58721
Dan Gurney <gurney.dan@gmail.com>
2021-08-05 17:37:15 +0100

    Add logs again

--- api/src/db.ts
+++ api/src/db.ts
@@ -2,6 +2,7 @@ import { createConnection, getConnectionManager } from "typeorm";
 import ormConfig, { DB_CONNECTION_NAME } from "ormconfig";
 
 export const connectToDatabase = () => {
+	console.log("ORM CONFIG", ormConfig);
 	return createConnection(ormConfig);
 
 	// Add 'unaccent' PostgreSQL extension to enable accent-insensitive queries,

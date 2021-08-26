1f71a5fa4a71f52c378c3a34602f9558f74e7c3c
Dan Gurney <gurney.dan@gmail.com>
2021-08-26 17:59:26 +0100

    Update envs to not used reserved

--- api/src/services/S3.ts
+++ api/src/services/S3.ts
@@ -1,19 +1,23 @@
 import AwsS3 from "aws-sdk/clients/s3";
 
-const { AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET } =
-	process.env;
+const {
+	API_AWS_REGION,
+	API_AWS_ACCESS_KEY_ID,
+	API_AWS_SECRET_ACCESS_KEY,
+	API_AWS_S3_BUCKET,
+} = process.env;
 
 const S3 = new AwsS3({
 	apiVersion: "2006-03-01",
-	region: AWS_REGION,
-	accessKeyId: AWS_ACCESS_KEY_ID,
-	secretAccessKey: AWS_SECRET_ACCESS_KEY,
+	region: API_AWS_REGION,
+	accessKeyId: API_AWS_ACCESS_KEY_ID,
+	secretAccessKey: API_AWS_SECRET_ACCESS_KEY,
 	signatureVersion: "v4",
 });
 
 const makePresignedPutUrl = (s3Key: string) => {
 	const params = {
-		Bucket: AWS_S3_BUCKET,
+		Bucket: API_AWS_S3_BUCKET,
 		Key: s3Key,
 		Expires: 360,
 	};
@@ -22,7 +26,7 @@ const makePresignedPutUrl = (s3Key: string) => {
 
 const makePresignedGetUrl = (s3Key: string) => {
 	const params = {
-		Bucket: AWS_S3_BUCKET,
+		Bucket: API_AWS_S3_BUCKET,
 		Key: s3Key,
 		Expires: 360,
 	};

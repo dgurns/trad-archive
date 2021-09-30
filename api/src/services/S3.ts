c7668aea4cb9a75d62a78be5a041b1715645ba48
Dan Gurney <gurney.dan@gmail.com>
2021-09-30 12:05:50 +0100

    Try configuring S3 client with envs

--- api/src/services/S3.ts
+++ api/src/services/S3.ts
@@ -6,12 +6,22 @@ import {
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const { API_AWS_S3_BUCKET } = process.env;
+const {
+	API_AWS_S3_BUCKET,
+	API_AWS_REGION,
+	API_AWS_ACCESS_KEY_ID,
+	API_AWS_SECRET_ACCESS_KEY,
+} = process.env;
 
 console.log("ACCESS KEY", process.env.AWS_ACCESS_KEY_ID);
 
 const s3Client = new S3Client({
 	apiVersion: "2006-03-01",
+	region: API_AWS_REGION ?? "",
+	credentials: {
+		accessKeyId: API_AWS_ACCESS_KEY_ID ?? "",
+		secretAccessKey: API_AWS_SECRET_ACCESS_KEY ?? "",
+	},
 });
 
 const makePresignedPutUrl = (s3Key: string) => {

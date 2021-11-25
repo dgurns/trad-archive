3de96a87750e784b820cecb7e45b5b4312c7dec3
Dan Gurney <gurney.dan@gmail.com>
2021-11-25 10:55:06 +0000

    Add a copy-pastable link to auto login email

--- api/src/services/Mailer.ts
+++ api/src/services/Mailer.ts
@@ -35,7 +35,19 @@ const sendEmailWithAutoLoginUrl = ({
 		from: "no-reply@tradarchive.com",
 		subject: "Your login for Trad Archive",
 		text: `Hi ${user.username}, click this link to log in to Trad Archive: ${autoLoginUrl} . It will be valid for the next 10 minutes. If you didn't request this, you can ignore this email.`,
-		html: `Hi ${user.username},<br /><br /><a href="${autoLoginUrl}">Click here to log in to Trad Archive</a><br /><br />This link will be valid for the next 10 minutes. If you didn't request it, you can ignore this email.`,
+		html: `
+			Hi ${user.username},
+			<br /><br />
+			<a href="${autoLoginUrl}">Click here to log in to Trad Archive</a>
+			<br /><br />
+			This link will be valid for the next 10 minutes.
+			<br /><br />
+			- - -
+			<br /><br />
+			If it doesn't work, copy and paste this URL into your browser:
+			<br /><br />
+			<em>${autoLoginUrl}</em>
+		`,
 	};
 
 	return sendEmail(data);

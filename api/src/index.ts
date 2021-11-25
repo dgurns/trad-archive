046031289ee288d36cb71aa13fe7e204f8e5bc99
Dan Gurney <gurney.dan@gmail.com>
2021-11-25 17:48:21 +0000

    Enable CORS from anywhere

--- api/src/index.ts
+++ api/src/index.ts
@@ -46,18 +46,19 @@ const start = async () => {
 			cors: {
 				// This cors policy only applies to requests going to the GraphQL API
 				origin: function (origin, callback) {
-					if (process.env.NODE_ENV === "development") {
-						return callback(null, true);
-					} else if (
-						origin &&
-						(origin.includes("https://www.tradarchive.com") ||
-							origin.includes("https://tradarchive.com") ||
-							origin.includes("-dangurney.vercel.app"))
-					) {
-						return callback(null, true);
-					} else {
-						return callback(new Error("Request blocked by CORS"));
-					}
+					return callback(null, true);
+					// if (process.env.NODE_ENV === "development") {
+					// 	return callback(null, true);
+					// } else if (
+					// 	origin &&
+					// 	(origin.includes("https://www.tradarchive.com") ||
+					// 		origin.includes("https://tradarchive.com") ||
+					// 		origin.includes("-dangurney.vercel.app"))
+					// ) {
+					// 	return callback(null, true);
+					// } else {
+					// 	return callback(new Error("Request blocked by CORS"));
+					// }
 				},
 				credentials: true,
 			},

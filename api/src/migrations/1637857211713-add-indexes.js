e8745e87f064af30fba6f3169930a02a149084b6
Dan Gurney <gurney.dan@gmail.com>
2021-11-25 18:31:33 +0000

    Fix migrations

--- api/src/migrations/1637857211713-add-indexes.js
+++ api/src/migrations/1637857211713-add-indexes.js
@@ -5,22 +5,22 @@ module.exports = class addIndexes1637857211713 {
 
 	async up(queryRunner) {
 		await queryRunner.query(
-			`CREATE INDEX "idx_gin_collection_search" ON "public"."collection" ("name", "aliases", "description") `
+			`CREATE INDEX IF NOT EXISTS "idx_gin_collection_search" ON "public"."collection" ("name", "aliases", "description") `
 		);
 		await queryRunner.query(
-			`CREATE INDEX "idx_gin_tune_search" ON "public"."tune" ("name", "aliases") `
+			`CREATE INDEX IF NOT EXISTS "idx_gin_tune_search" ON "public"."tune" ("name", "aliases") `
 		);
 		await queryRunner.query(
-			`CREATE INDEX "idx_gin_place_search" ON "public"."place" ("name", "aliases", "description") `
+			`CREATE INDEX IF NOT EXISTS "idx_gin_place_search" ON "public"."place" ("name", "aliases", "description") `
 		);
 		await queryRunner.query(
-			`CREATE INDEX "idx_gin_instrument_search" ON "public"."instrument" ("name", "aliases", "description") `
+			`CREATE INDEX IF NOT EXISTS "idx_gin_instrument_search" ON "public"."instrument" ("name", "aliases", "description") `
 		);
 		await queryRunner.query(
-			`CREATE INDEX "idx_gin_person_search" ON "public"."person" ("name", "aliases", "description") `
+			`CREATE INDEX IF NOT EXISTS "idx_gin_person_search" ON "public"."person" ("name", "aliases", "description") `
 		);
 		await queryRunner.query(
-			`CREATE INDEX "idx_gin_audio_item_search" ON "public"."audio_item" ("name", "aliases", "description") `
+			`CREATE INDEX IF NOT EXISTS "idx_gin_audio_item_search" ON "public"."audio_item" ("name", "aliases", "description") `
 		);
 	}
 

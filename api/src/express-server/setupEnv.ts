2877be512abd69f246f6b69e758345f4bbd8f202
Dan Gurney <gurney.dan@gmail.com>
2021-11-27 19:20:20 +0000

    Don't prefetch fonts

--- api/src/express-server/setupEnv.ts
+++ api/src/express-server/setupEnv.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import path from "path";
 
-// Find the .env file and load it into process.env
+// Find the .env file and load it into process.env. This particular .env path in
+// production is specific to deploying on Render.com
 const envPath =
 	process.env.NODE_ENV === "development"
 		? path.resolve(".env")
--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -40,17 +40,7 @@ class MyDocument extends Document {
 					<link rel="preconnect" href="https://fonts.gstatic.com/" />
 					<link
 						href="https://fonts.googleapis.com/icon?family=Material+Icons"
-						rel="preload"
-						as="style"
-					/>
-					<link
-						href="https://fonts.googleapis.com/icon?family=Material+Icons"
 						rel="stylesheet"
-					/>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap"
-						rel="preload"
-						as="style"
 					/>
 					<link
 						href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap"

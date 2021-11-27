4d2f8532c35a04c2565e6f694a370be0407c5ebb
Dan Gurney <gurney.dan@gmail.com>
2021-11-27 18:46:18 +0000

    Enable Render to find env file

--- api/src/express-server/setupEnv.ts
+++ api/src/express-server/setupEnv.ts
@@ -2,5 +2,8 @@ import dotenv from "dotenv";
 import path from "path";
 
 // Find the .env file and load it into process.env
-const envPath = path.resolve(".env");
+const envPath =
+	process.env.NODE_ENV === "development"
+		? path.resolve(".env")
+		: "/etc/secrets/.env";
 dotenv.config({ path: envPath });

ffae6cc25a3bcca4e736e11d477a94e3f5df5adf
Dan Gurney <gurney.dan@gmail.com>
2022-01-01 17:58:09 +0000

    Begin building out SSM service

---
+++ api/src/services/SSM.ts
@@ -0,0 +1,29 @@
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+
+const {
+	API_AWS_REGION = "",
+	API_AWS_ACCESS_KEY_ID = "",
+	API_AWS_SECRET_ACCESS_KEY = "",
+} = process.env;
+
+const ssmClient = new SSMClient({
+	apiVersion: "2006-03-01",
+	region: API_AWS_REGION,
+	credentials: {
+		accessKeyId: API_AWS_ACCESS_KEY_ID,
+		secretAccessKey: API_AWS_SECRET_ACCESS_KEY,
+	},
+});
+
+const getParameter = (parameterName: string) => {
+	const command = new GetParameterCommand({
+		Name: parameterName,
+		WithDecryption: true,
+	});
+	return ssmClient.send(command);
+};
+
+const SSMService = {
+	getParameter,
+};
+export default SSMService;

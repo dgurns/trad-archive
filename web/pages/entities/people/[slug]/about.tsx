afcb6dffdcd322aba7a4785c123350647ed157ed
Dan Gurney <gurney.dan@gmail.com>
2022-02-17 12:25:33 +0000

    Finish refactoring About and Tags pages

--- web/pages/entities/people/[slug]/about.tsx
+++ web/pages/entities/people/[slug]/about.tsx
@@ -1,93 +1,60 @@
-import {
-	ApolloClient,
-	InMemoryCache,
-	NormalizedCacheObject,
-} from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import { Person } from "types";
-import { API_URL } from "apolloClient";
 
 import Layout from "components/Layout";
 import { PERSON_QUERY } from "pages/entities/people/[slug]";
 import Breadcrumb from "components/Breadcrumb";
+import LoadingBlock from "components/LoadingBlock";
 
-// Attempt to reuse instance of server side Apollo Client between runs of
-// getStaticProps, to avoid creating a new DB connection on every request
-let serverSideApolloClient: ApolloClient<NormalizedCacheObject> | undefined;
+const PersonAbout = () => {
+	const router = useRouter();
+	const { slug } = router.query;
 
-export async function getServerSideProps(context) {
-	let person: Person | undefined;
-
-	try {
-		if (!serverSideApolloClient) {
-			serverSideApolloClient = new ApolloClient({
-				uri: API_URL,
-				credentials: "include",
-				cache: new InMemoryCache(),
-				defaultOptions: {
-					query: {
-						// Force server-side queries to get the latest data each time
-						fetchPolicy: "no-cache",
-					},
-				},
-			});
-		}
-
-		const { data } = await serverSideApolloClient.query<{
-			person: Person;
-		}>({
-			query: PERSON_QUERY,
-			variables: { slug: context.params.slug },
-		});
-		person = data.person;
-	} catch {
-		//
-	}
-	return {
-		props: {
-			person,
-		},
-	};
-}
-
-interface Props {
-	person: Person;
-}
-
-const PersonAbout = ({ person }: Props) => {
-	if (!person) {
-		return <Layout>Error fetching Person</Layout>;
-	}
-
-	const { name, slug, description, aliases } = person;
+	const { data, error } = useQuery<{
+		person: Person;
+	}>(PERSON_QUERY, {
+		variables: { slug },
+		skip: !slug,
+	});
+	const isLoading = !data && !error;
+	const { name, description, aliases } = data?.person ?? {};
 
 	return (
 		<Layout pageTitle={`Trad Archive - ${name} - About`}>
-			<Breadcrumb
-				items={[
-					{ label: "People", href: "/entities/people" },
-					{ label: name, href: `/entities/people/${slug}` },
-					{ label: "About" },
-				]}
-				className="mb-6"
-			/>
-
-			{description && (
-				<div className="mb-4">
-					Description:
-					<br />
-					<span className="text-gray-500">{description}</span>
-				</div>
+			{isLoading && <LoadingBlock />}
+			{error && <div className="text-red-500">{error.message}</div>}
+			{data && (
+				<>
+					<Breadcrumb
+						items={[
+							{ label: "People", href: "/entities/people" },
+							{ label: name, href: `/entities/people/${slug}` },
+							{ label: "About" },
+						]}
+						className="mb-6"
+					/>
+					{description && (
+						<div className="mb-4">
+							Description:
+							<br />
+							<span className="text-gray-500 whitespace-pre">
+								{description}
+							</span>
+						</div>
+					)}
+					{aliases && (
+						<div className="mb-4">
+							Aliases:
+							<br />
+							<span className="text-gray-500">{aliases}</span>
+						</div>
+					)}
+					<Link href={`/entities/people/${slug}/edit`}>Edit</Link>
+				</>
 			)}
-			{aliases && (
-				<div className="mb-4">
-					Aliases:
-					<br />
-					<span className="text-gray-500">{aliases}</span>
-				</div>
-			)}
-			<Link href={`/entities/people/${slug}/edit`}>Edit</Link>
 		</Layout>
 	);
 };
--- web/pages/entities/tunes/[slug]/tags.tsx
+++ web/pages/entities/tunes/[slug]/tags.tsx
@@ -1,100 +1,65 @@
-import {
-	ApolloClient,
-	InMemoryCache,
-	NormalizedCacheObject,
-} from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
 
 import { Tune } from "types";
 import TagService from "services/Tag";
-import { API_URL } from "apolloClient";
 import { TUNE_QUERY } from "pages/entities/tunes/[slug]";
 
 import Layout from "components/Layout";
 import Breadcrumb from "components/Breadcrumb";
+import LoadingBlock from "components/LoadingBlock";
 import TagWithRelationshipToObject from "components/TagWithRelationshipToObject";
 import AddTagButton from "components/AddTagButton";
 import EditTagsButton from "components/EditTagsButton";
 
-// Attempt to reuse instance of server side Apollo Client between runs of
-// getStaticProps, to avoid creating a new DB connection on every request
-let serverSideApolloClient: ApolloClient<NormalizedCacheObject> | undefined;
+const TuneTags = () => {
+	const router = useRouter();
+	const { slug } = router.query;
 
-export async function getServerSideProps(context) {
-	let tune: Tune | undefined;
-
-	try {
-		if (!serverSideApolloClient) {
-			serverSideApolloClient = new ApolloClient({
-				uri: API_URL,
-				credentials: "include",
-				cache: new InMemoryCache(),
-				defaultOptions: {
-					query: {
-						// Force server-side queries to get the latest data each time
-						fetchPolicy: "no-cache",
-					},
-				},
-			});
-		}
-
-		const { data } = await serverSideApolloClient.query<{
-			tune: Tune;
-		}>({
-			query: TUNE_QUERY,
-			variables: { slug: context.params.slug },
-		});
-		tune = data.tune;
-	} catch {
-		//
-	}
-	return {
-		props: {
-			tune,
-		},
-	};
-}
-
-interface Props {
-	tune: Tune;
-}
-
-const TuneTags = ({ tune }: Props) => {
-	if (!tune) {
-		return <Layout>Error fetching Tune</Layout>;
-	}
-
-	const { name, slug, tags } = tune;
+	const { data, error, refetch } = useQuery<{
+		tune: Tune;
+	}>(TUNE_QUERY, {
+		variables: { slug },
+		skip: !slug,
+	});
+	const isLoading = !data && !error;
+	const { tune } = data ?? {};
+	const { name, tags } = tune ?? {};
 	const sortedTags = TagService.sort(tags);
 
 	return (
 		<Layout pageTitle={`Trad Archive - ${name} - Tags`}>
-			<Breadcrumb
-				items={[
-					{ label: "Tunes", href: "/entities/tunes" },
-					{ label: name, href: `/entities/tunes/${slug}` },
-					{ label: "Tags" },
-				]}
-				className="mb-6"
-			/>
+			{isLoading && <LoadingBlock />}
+			{error && <div className="text-red-500">{error.message}</div>}
+			{data && (
+				<>
+					<Breadcrumb
+						items={[
+							{ label: "Tunes", href: "/entities/tunes" },
+							{ label: name, href: `/entities/tunes/${slug}` },
+							{ label: "Tags" },
+						]}
+						className="mb-6"
+					/>
 
-			{sortedTags.map((tag, index) => (
-				<TagWithRelationshipToObject tag={tag} key={index} className="mb-4" />
-			))}
-			<div>
-				<AddTagButton
-					entity={tune}
-					onSuccess={() => window.location.reload()}
-				/>
-				{sortedTags.length > 0 && (
-					<>
-						<span className="text-gray-500 px-2">/</span>
-						<EditTagsButton
-							entity={tune}
-							onSuccess={() => window.location.reload()}
+					{sortedTags.map((tag, index) => (
+						<TagWithRelationshipToObject
+							tag={tag}
+							key={index}
+							className="mb-4"
 						/>
-					</>
-				)}
-			</div>
+					))}
+					<div>
+						<AddTagButton entity={tune} onSuccess={refetch} />
+						{sortedTags.length > 0 && (
+							<>
+								<span className="text-gray-500 px-2">/</span>
+								<EditTagsButton entity={tune} onSuccess={refetch} />
+							</>
+						)}
+					</div>
+				</>
+			)}
 		</Layout>
 	);
 };

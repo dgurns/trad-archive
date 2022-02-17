c44633642398276e5a57e1b3c58521b26be720ad
Dan Gurney <gurney.dan@gmail.com>
2022-02-17 18:19:07 +0000

    Add tooltip with Tag information

--- web/components/Tags.tsx
+++ web/components/Tags.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState, useCallback, useEffect } from "react";
 import Link from "next/link";
 
 import { Tag, Entity } from "types";
@@ -12,12 +12,48 @@ interface TagProps {
 	tag: Tag;
 }
 const TagLink = ({ tag }: TagProps) => {
-	const { objectEntity } = tag;
+	const [tooltipIsVisible, setTooltipIsVisible] = useState(false);
+	const [timeoutFunc, setTimeoutFunc] = useState<NodeJS.Timeout>();
+
+	const onMouseEnter = useCallback(() => {
+		setTimeoutFunc(setTimeout(() => setTooltipIsVisible(true), 400));
+	}, []);
+
+	const onMouseLeave = useCallback(() => {
+		if (timeoutFunc) {
+			clearTimeout(timeoutFunc);
+			setTimeoutFunc(undefined);
+		}
+		setTooltipIsVisible(false);
+	}, [timeoutFunc]);
+
+	// Clean up the timeout function when component unmounts
+	useEffect(() => {
+		return () => {
+			if (timeoutFunc) {
+				clearTimeout(timeoutFunc);
+				setTimeoutFunc(undefined);
+			}
+		};
+	}, [timeoutFunc]);
+
+	const { objectEntity, relationship } = tag;
 	const href = EntityService.makeHrefForView(objectEntity);
 	return (
 		<Link href={href}>
-			<a className="block p-1 px-2 mb-2 no-underline border border-teal-600 rounded hover:border-teal-800">
+			<a
+				className="relative block p-1 px-2 mb-2 no-underline border border-teal-600 rounded hover:border-teal-800"
+				onMouseEnter={onMouseEnter}
+				onMouseLeave={onMouseLeave}
+			>
 				{objectEntity.name}
+				<div
+					className={`${
+						tooltipIsVisible ? "flex" : "hidden"
+					} absolute -top-8 left-0 text-center px-2 py-1 text-sm whitespace-nowrap bg-gray-700 rounded text-white`}
+				>
+					{relationship.name} {objectEntity.entityType.toUpperCase()}
+				</div>
 			</a>
 		</Link>
 	);

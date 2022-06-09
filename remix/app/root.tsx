02548e700ff288001e40ce97f4bd21b0b78e5e6e
Dan Gurney <gurney.dan@gmail.com>
2022-06-09 11:10:17 +0100

    Set up Remix project

---
+++ remix/app/root.tsx
@@ -0,0 +1,43 @@
+import type { MetaFunction } from "@remix-run/cloudflare";
+import {
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from "@remix-run/react";
+import styles from "~/styles/globals-generated-do-not-edit.css";
+
+export const meta: MetaFunction = () => ({
+	charset: "utf-8",
+	title: "Trad Archive",
+	viewport: "width=device-width,initial-scale=1",
+});
+
+export function links() {
+	return [
+		{ rel: "stylesheet", href: styles },
+		{
+			rel: "shortcut icon",
+			href: "/favicon.ico",
+		},
+	];
+}
+
+export default function App() {
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
+}

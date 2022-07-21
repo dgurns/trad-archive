c5a5dc312ef8cd037d31a8f95a7cf6b2c72a3015
Dan Gurney <gurney.dan@gmail.com>
2022-07-21 11:01:20 +0100

    Fix saved-items page

--- remix/app/routes/saved-items.tsx
+++ remix/app/routes/saved-items.tsx
@@ -1,16 +1,66 @@
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+import { json, redirect, type LoaderFunction } from "@remix-run/node";
 
-import useSavedItemsForUser from "~/hooks/useSavedItemsForUser";
 import useFilters from "~/hooks/useFilters";
-import { ViewAs } from "~/types";
+import { ViewAs, type SavedItemWithRelations } from "~/types";
+import { db } from "~/utils/db.server";
+import { getSession } from "~/sessions.server";
 
 import Layout from "~/components/Layout";
-import RequireUser from "~/components/RequireUser";
-import LoadingBlock from "~/components/LoadingBlock";
 import AudioItem from "~/components/AudioItem";
 
-const SavedItems = () => {
-	const [savedItems, { loading, error }] = useSavedItemsForUser();
+interface LoaderData {
+	savedItems: SavedItemWithRelations[];
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+	const session = await getSession(request.headers.get("Cookie"));
+	const userId = String(session.get("userId") ?? "");
+
+	const { pathname, searchParams } = new URL(request.url);
+	const redirectParams = new URLSearchParams({
+		redirectTo: pathname,
+	});
+	if (!userId) {
+		return redirect(`/login?${redirectParams.toString()}`);
+	}
+
+	const page = Number(searchParams.get("page") ?? 1);
+	const perPage = Number(searchParams.get("perPage") ?? 20);
+	const savedItems = await db.savedItem.findMany({
+		where: { userId },
+		include: {
+			audioItem: {
+				include: {
+					tagsAsSubject: {
+						include: {
+							objectAudioItem: true,
+							objectCollection: true,
+							objectInstrument: true,
+							objectPerson: true,
+							objectPlace: true,
+							objectTune: true,
+							relationship: true,
+						},
+					},
+					createdByUser: true,
+					updatedByUser: true,
+					comments: {
+						include: {
+							createdByUser: true,
+						},
+					},
+				},
+			},
+		},
+		skip: (page - 1) * perPage,
+		take: perPage,
+	});
+	return json<LoaderData>({ savedItems });
+};
+
+export default function SavedItems() {
+	const { savedItems } = useLoaderData<LoaderData>();
 
 	const { Filters, filtersProps, viewAs } = useFilters({
 		defaultViewAs: ViewAs.List,
@@ -18,40 +68,29 @@ const SavedItems = () => {
 
 	return (
 		<Layout>
-			<RequireUser>
-				<div className="flex flex-col">
-					<h1 className="mb-6">Saved Items</h1>
+			<div className="flex flex-col">
+				<h1 className="mb-6">Saved Items</h1>
 
-					{savedItems?.length > 0 && (
-						<Filters {...filtersProps} className="mb-6" />
-					)}
+				{savedItems.length > 0 && (
+					<Filters {...filtersProps} className="mb-6" />
+				)}
 
-					{error && (
-						<div className="text-red-600 mb-4">Could not fetch saved items</div>
-					)}
-					{loading && !savedItems && (
-						<div className="mb-4">
-							<LoadingBlock />
-						</div>
-					)}
-					{!loading && savedItems?.length === 0 && (
-						<div className="text-gray-500">
-							Nothing saved yet - try browsing some{" "}
-							<Link to="/">Audio Items</Link>!
-						</div>
-					)}
-					{savedItems?.map(({ audioItem }, index) => (
-						<AudioItem
-							viewAs={viewAs}
-							audioItem={audioItem}
-							key={index}
-							className={viewAs === ViewAs.List ? "mb-4" : "mb-6"}
-						/>
-					))}
-				</div>
-			</RequireUser>
+				{savedItems.length === 0 && (
+					<div className="text-gray-500">
+						Nothing saved yet - try browsing some{" "}
+						<Link to="/">Audio Items</Link>!
+					</div>
+				)}
+
+				{savedItems.map(({ audioItem }, index) => (
+					<AudioItem
+						viewAs={viewAs}
+						audioItem={audioItem}
+						key={index}
+						className={viewAs === ViewAs.List ? "mb-4" : "mb-6"}
+					/>
+				))}
+			</div>
 		</Layout>
 	);
-};
-
-export default SavedItems;
+}

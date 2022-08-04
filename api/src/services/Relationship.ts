9bf46cd15e1835e1a4882b00432f77336552cedc
Dan Gurney <gurney.dan@gmail.com>
2022-08-04 12:14:51 +0100

    Remove old app

--- api/src/services/Relationship.ts
+++
@@ -1,9 +0,0 @@
-const cleanName = (rawName: string) => {
-  // Remove any character that is not a letter or space
-  return rawName.replace(/[^a-zA-Z\s]/g, '').toLowerCase();
-};
-
-const RelationshipService = {
-  cleanName,
-};
-export default RelationshipService;
--- web/hooks/usePlayerContext.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { PlayerContext } from "components/PlayerContextProvider";
-
-const usePlayerContext = () => {
-	const playerContext = useContext(PlayerContext);
-	return playerContext;
-};
-
-export default usePlayerContext;

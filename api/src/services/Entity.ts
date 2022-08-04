1a4dcc9f66c536f3d05f39085b9ec04cec48052b
Dan Gurney <gurney.dan@gmail.com>
2022-08-04 12:20:39 +0100

    Update Prisma schema and remove old code (#78)

--- api/src/services/Entity.ts
+++
@@ -1,8 +0,0 @@
-const cleanSlug = (rawSlug: string) => {
-  return rawSlug.replace(/[^a-zA-Z0-9-]/g, '').toLowerCase();
-};
-
-const EntityService = {
-  cleanSlug,
-};
-export default EntityService;

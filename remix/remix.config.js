ff08017de2c68b36e251c8b274822fc91528fa25
Dan Gurney <dan@dangurney.net>
2023-04-22 14:43:53 +0100

    fix: build targets

--- remix/remix.config.js
+++ remix/remix.config.js
@@ -2,14 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-	publicPath: process.env.NODE_ENV === "development" ? undefined : "/build/",
-	serverBuildPath:
-		process.env.NODE_ENV === "development" ? undefined : "api/index.js",
-	serverMainFields:
-		process.env.NODE_ENV === "development" ? undefined : "main, module",
-	serverModuleFormat:
-		process.env.NODE_ENV === "development" ? undefined : "cjs",
-	serverPlatform: process.env.NODE_ENV === "development" ? undefined : "node",
+	publicPath: "/build/",
+	serverBuildPath: "api/index.js",
+	serverMainFields: ["main", "module"],
+	serverModuleFormat: "cjs",
+	serverPlatform: "node",
 	serverMinify: false,
 	ignoredRouteFiles: ["**/.*"],
 	future: {

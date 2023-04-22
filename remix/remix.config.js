61e2796b30359171cbf73a9c6d7f6677acd293b8
Dan Gurney <dan@dangurney.net>
2023-04-22 17:22:40 +0100

    revert to serverBuildTarget

--- remix/remix.config.js
+++ remix/remix.config.js
@@ -2,12 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-	ignoredRouteFiles: ["**/.*"],
-	// When running locally in development mode, we use the built-in remix
+	serverBuildTarget: "vercel",
+	// When running locally in development mode, we use the built in remix
 	// server. This does not understand the vercel lambda module format,
 	// so we default back to the standard build output.
-	server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
-	serverBuildPath: "api/index.js",
+	server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+	ignoredRouteFiles: ["**/.*"],
 	future: {
 		v2_errorBoundary: true,
 		v2_normalizeFormMethod: true,

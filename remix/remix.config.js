cc76a512723d404128a3ffdc2286e272f4b67734
Dan Gurney <dan@dangurney.net>
2023-04-22 16:31:49 +0100

    add to gitignore

--- remix/remix.config.js
+++ remix/remix.config.js
@@ -2,12 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-	publicPath: "/build/",
 	serverBuildPath: "api/index.js",
-	serverMainFields: ["main", "module"],
-	serverModuleFormat: "cjs",
-	serverPlatform: "node",
-	serverMinify: false,
+	// When running locally in development mode, we use the built in remix
+	// server. This does not understand the vercel lambda module format,
+	// so we default back to the standard build output.
+	server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
 	ignoredRouteFiles: ["**/.*"],
 	future: {
 		v2_errorBoundary: true,
